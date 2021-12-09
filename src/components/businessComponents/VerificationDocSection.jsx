a83a34a58c702e5da392c3fe2ce5c33202363920
Dung Lu <dung.lu@kyanon.digital>
2021-12-09 11:12:35 +0700

    Update

--- src/components/businessComponents/VerificationDocSection.jsx
+++ src/components/businessComponents/VerificationDocSection.jsx
@@ -4,13 +4,10 @@ import {
 } from '@components/uiComponents';
 import DocumentType from '@constants/DocumentType';
 import Theme from '@constants/Theme';
-import VerificationStatus from '@constants/VerificationStatus';
-import MediaHelpers from '@helpers/MediaHelpers';
 import ToastHelpers from '@helpers/ToastHelpers';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import ImageScalable from 'react-native-scalable-image';
-import { useSelector } from 'react-redux';
 
 const {
     FONT: {
@@ -25,8 +22,6 @@ export default function UploadDocSection({ verificationDocuments }) {
     const [frontUrl, setFrontUrl] = useState('');
     const [backUrl, setBackUrl] = useState('');
     const [greenUrl, setGreenUrl] = useState('');
-
-    const currentUser = useSelector((state) => state.userReducer.currentUser);
 
     useEffect(
         () => {
@@ -64,69 +59,26 @@ export default function UploadDocSection({ verificationDocuments }) {
         return true;
     };
 
-    const onPickVerificationDoc = (docType) => {
-        MediaHelpers.pickImage(
-            false,
-            [4, 3],
-            (result) => {
-                handleOnPickVerificationDoc(result.uri, docType);
-            },
-            0.6
-        );
-    };
-
-    const handleOnPickVerificationDoc = (uri, docType) => {
-        switch (docType) {
-            case DocumentType.FACE_IMAGE: {
-                setFaceUrl(uri);
-                break;
-            }
-            case DocumentType.ID_CARD_FRONT: {
-                setFrontUrl(uri);
-                break;
-            }
-            case DocumentType.ID_CARD_BACK: {
-                setBackUrl(uri);
-                break;
-            }
-            case DocumentType.GREEN_CARD: {
-                setGreenUrl(uri);
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-    };
-
-    const renderUploadDocForm = (docType, buttonText) => {
-        let isDisabled = false;
-        if (currentUser?.verifyStatus !== VerificationStatus.NONE
-            && currentUser?.verifyStatus !== VerificationStatus.REJECT) {
-            isDisabled = true;
-        }
-        return (
-            <View style={{
-                alignItems: 'center',
-            }}
-            >
-                <CustomButton
-                    onPress={() => onPickVerificationDoc(docType)}
-                    type="active"
-                    label={buttonText}
-                    buttonStyle={{
-                        width: SIZES.WIDTH_BASE * 0.9,
-                        marginBottom: 10
-                    }}
-                    labelStyle={{
-                        fontFamily: TEXT_REGULAR,
-                        fontSize: SIZES.FONT_H4
-                    }}
-                    disabled={isDisabled}
-                />
-            </View>
-        );
-    };
+    const renderDocTitle = (docType, buttonText) => (
+        <View style={{
+            alignItems: 'center',
+        }}
+        >
+            <CustomButton
+                type="active"
+                label={buttonText}
+                buttonStyle={{
+                    width: SIZES.WIDTH_BASE * 0.9,
+                    marginBottom: 10
+                }}
+                labelStyle={{
+                    fontFamily: TEXT_REGULAR,
+                    fontSize: SIZES.FONT_H4
+                }}
+                disabled
+            />
+        </View>
+    );
 
     const renderDocSection = () => (
         <View
@@ -134,24 +86,24 @@ export default function UploadDocSection({ verificationDocuments }) {
                 marginVertical: 10
             }}
         >
-            {renderUploadDocForm(DocumentType.FACE_IMAGE, 'Ảnh chụp chính diện')}
+            {renderDocTitle(DocumentType.FACE_IMAGE, 'Ảnh chụp chính diện')}
             {renderDocImage(faceUrl)}
             <View
                 style={styles.docFormContainer}
             >
-                {renderUploadDocForm(DocumentType.ID_CARD_FRONT, 'Ảnh chụp nhìn sang phải')}
+                {renderDocTitle(DocumentType.ID_CARD_FRONT, 'Ảnh chụp nhìn sang phải')}
                 {renderDocImage(frontUrl)}
             </View>
             <View
                 style={styles.docFormContainer}
             >
-                {renderUploadDocForm(DocumentType.ID_CARD_BACK, 'Ảnh chụp mặt trước giấy phép lái xe')}
+                {renderDocTitle(DocumentType.ID_CARD_BACK, 'Ảnh chụp mặt trước giấy phép lái xe')}
                 {renderDocImage(backUrl)}
             </View>
             <View
                 style={styles.docFormContainer}
             >
-                {renderUploadDocForm(DocumentType.GREEN_CARD, 'Thẻ xanh COVID')}
+                {renderDocTitle(DocumentType.GREEN_CARD, 'Thẻ xanh COVID')}
                 {renderDocImage(greenUrl)}
             </View>
         </View>
