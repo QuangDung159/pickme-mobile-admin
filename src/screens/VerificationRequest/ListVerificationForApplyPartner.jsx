435ab768553b2ae61679eb5dde4c73e87bfe0bfe
Dung Lu <dung.lu@kyanon.digital>
2021-12-09 11:56:19 +0700

    Update

---
+++ src/screens/VerificationRequest/ListVerificationForApplyPartner.jsx
@@ -0,0 +1,191 @@
+import {
+    CenterLoader, CustomText
+} from '@components/uiComponents';
+import { ScreenName, Theme } from '@constants/index';
+import { CommonHelpers, ToastHelpers } from '@helpers/index';
+import { UserServices } from '@services/index';
+import React, { useEffect, useState } from 'react';
+import {
+    RefreshControl, ScrollView, TouchableOpacity, View
+} from 'react-native';
+
+const {
+    SIZES,
+    COLORS,
+    FONT: {
+        TEXT_BOLD
+    }
+} = Theme;
+
+export default function ListVerificationForApplyPartner({ navigation }) {
+    const [isShowSpinner, setIsShowSpinner] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
+    const [listVerification, setListVerification] = useState();
+    const [verificationSelected, setVerificationSelected] = useState();
+
+    useEffect(
+        () => {
+            const onFocus = navigation.addListener('focus', () => {
+                setIsShowSpinner(true);
+                getListWaitingVerificationRequest();
+            });
+
+            return onFocus;
+        }, []
+    );
+
+    useEffect(
+        () => {
+            if (verificationSelected) {
+                navigation.navigate(ScreenName.VERIFICATION_REQUEST_DETAIL, {
+                    verification: verificationSelected
+                });
+            }
+        }, [verificationSelected]
+    );
+
+    const getListWaitingVerificationRequest = async () => {
+        const res = await UserServices.fetchListVerificationRequestAsync({
+            isApplyForPartner: true,
+        });
+        const { data } = res;
+
+        if (data) {
+            let listNotComplete = [];
+            listNotComplete = data.data.filter((item) => !item.isCompleted);
+            setListVerification(listNotComplete);
+
+            setIsShowSpinner(false);
+            setRefreshing(false);
+        }
+    };
+
+    const renderVerificationRequestItem = (item) => (
+        <TouchableOpacity
+            onPress={
+                () => setVerificationSelected(item)
+            }
+        >
+            <View
+                style={{
+                    width: SIZES.WIDTH_BASE * 0.95,
+                    alignSelf: 'center',
+                    borderColor: COLORS.ACTIVE,
+                    borderRadius: 20,
+                    borderWidth: 1,
+                    marginBottom: 10,
+                }}
+            >
+                <View
+                    style={{
+                        width: SIZES.WIDTH_BASE * 0.9,
+                        alignSelf: 'center',
+                        marginVertical: 10
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}
+                    >
+                        <View
+                            style={{
+                                width: SIZES.WIDTH_BASE * 0.4
+                            }}
+                        >
+                            <CustomText
+                                text="Tên đăng nhập:"
+                            />
+                            <CustomText
+                                text="Ngày yêu cầu:"
+                            />
+                        </View>
+                        <View
+                            style={{
+                                width: SIZES.WIDTH_BASE * 0.5
+                            }}
+                        >
+                            <CustomText
+                                style={{
+                                    fontFamily: TEXT_BOLD
+                                }}
+                                text={`${item.userName}`}
+                            />
+                            <CustomText
+                                style={{
+                                    fontFamily: TEXT_BOLD
+                                }}
+                                text={`${CommonHelpers.formatTime(item.applyDate)}`}
+                            />
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+
+    const onRefresh = () => {
+        getListWaitingVerificationRequest();
+    };
+
+    const renderListWaitingVerificationRequest = () => (
+        <View
+            style={{
+                marginTop: 10
+            }}
+        >
+            {listVerification && listVerification.length > 0 ? (
+                <>
+                    {listVerification.map((item) => (
+                        <View
+                            key={item.id}
+                        >
+                            {renderVerificationRequestItem(item)}
+                        </View>
+                    ))}
+                </>
+            ) : (
+                <CustomText
+                    style={{
+                        textAlign: 'center',
+                    }}
+                    text="Không có dữ liệu"
+                />
+            )}
+        </View>
+    );
+
+    try {
+        return (
+            <>
+                {isShowSpinner ? (
+                    <CenterLoader />
+                ) : (
+                    <ScrollView
+                        contentContainerStyle={{
+                            width: SIZES.WIDTH_BASE,
+                            minHeight: SIZES.HEIGHT_BASE
+                        }}
+                        refreshControl={(
+                            <RefreshControl
+                                refreshing={refreshing}
+                                onRefresh={() => onRefresh()}
+                                tintColor={COLORS.ACTIVE}
+                            />
+                        )}
+                    >
+                        {renderListWaitingVerificationRequest()}
+                    </ScrollView>
+                )}
+            </>
+        );
+    } catch (exception) {
+        console.log('exception :>> ', exception);
+        return (
+            <>
+                {ToastHelpers.renderToast()}
+            </>
+        );
+    }
+}
--- src/screens/VerificationRequest/index.jsx
+++ src/screens/VerificationRequest/index.jsx
@@ -1,184 +1,36 @@
-import {
-    CenterLoader, CustomText
-} from '@components/uiComponents';
-import { ScreenName, Theme } from '@constants/index';
-import { CommonHelpers, ToastHelpers } from '@helpers/index';
-import { UserServices } from '@services/index';
-import React, { useEffect, useState } from 'react';
-import {
-    RefreshControl, ScrollView, TouchableOpacity, View
-} from 'react-native';
+import { TopTabBar } from '@components/uiComponents';
+import { ToastHelpers } from '@helpers/index';
+import React, { useState } from 'react';
+import { SceneMap } from 'react-native-tab-view';
+import ListVerificationForApplyCustomer from './ListVerificationForApplyCustomer';
+import ListVerificationForApplyPartner from './ListVerificationForApplyPartner';
 
-const {
-    SIZES,
-    COLORS,
-    FONT: {
-        TEXT_BOLD
-    }
-} = Theme;
+export default function VerificationRequest({ navigation, route }) {
+    const [routes] = useState([
+        { key: 'listVerificationForApplyCustomer', title: 'Cho Customer' },
+        { key: 'listVerificationForApplyPartner', title: 'Cho Host' },
+    ]);
 
-export default function VerificationRequest({ navigation }) {
-    const [isShowSpinner, setIsShowSpinner] = useState(false);
-    const [refreshing, setRefreshing] = useState(false);
-    const [listVerification, setListVerification] = useState();
-    const [verificationSelected, setVerificationSelected] = useState();
-
-    useEffect(
-        () => {
-            const onFocus = navigation.addListener('focus', () => {
-                setIsShowSpinner(true);
-                getListWaitingVerificationRequest();
-            });
-
-            return onFocus;
-        }, []
+    const listVerificationForApplyPartnerRoute = () => (
+        <ListVerificationForApplyPartner navigation={navigation} />
     );
 
-    useEffect(
-        () => {
-            if (verificationSelected) {
-                navigation.navigate(ScreenName.VERIFICATION_REQUEST_DETAIL, {
-                    verification: verificationSelected
-                });
-            }
-        }, [verificationSelected]
+    const listVerificationForApplyCustomerRoute = () => (
+        <ListVerificationForApplyCustomer navigation={navigation} />
     );
 
-    const getListWaitingVerificationRequest = async () => {
-        const res = await UserServices.fetchListVerificationRequestAsync({
-            isApplyForPartner: true,
-        });
-        const { data } = res;
-
-        if (data) {
-            let listNotComplete = [];
-            listNotComplete = data.data.filter((item) => !item.isCompleted);
-            setListVerification(listNotComplete);
-
-            setIsShowSpinner(false);
-            setRefreshing(false);
-        }
-    };
-
-    const renderVerificationRequestItem = (item) => (
-        <TouchableOpacity
-            onPress={
-                () => setVerificationSelected(item)
-            }
-        >
-            <View
-                style={{
-                    width: SIZES.WIDTH_BASE * 0.95,
-                    alignSelf: 'center',
-                    borderColor: COLORS.ACTIVE,
-                    borderRadius: 20,
-                    borderWidth: 1,
-                    marginBottom: 10,
-                }}
-            >
-                <View
-                    style={{
-                        width: SIZES.WIDTH_BASE * 0.9,
-                        alignSelf: 'center',
-                        marginVertical: 10
-                    }}
-                >
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <View
-                            style={{
-                                width: SIZES.WIDTH_BASE * 0.4
-                            }}
-                        >
-                            <CustomText
-                                text="Tên đăng nhập:"
-                            />
-                            <CustomText
-                                text="Ngày yêu cầu:"
-                            />
-                        </View>
-                        <View
-                            style={{
-                                width: SIZES.WIDTH_BASE * 0.5
-                            }}
-                        >
-                            <CustomText
-                                style={{
-                                    fontFamily: TEXT_BOLD
-                                }}
-                                text={`${item.userName}`}
-                            />
-                            <CustomText
-                                style={{
-                                    fontFamily: TEXT_BOLD
-                                }}
-                                text={`${CommonHelpers.formatTime(item.applyDate)}`}
-                            />
-                        </View>
-                    </View>
-                </View>
-            </View>
-        </TouchableOpacity>
-    );
-
-    const onRefresh = () => {
-        getListWaitingVerificationRequest();
-    };
-
-    const renderListWaitingVerificationRequest = () => (
-        <View
-            style={{
-                marginTop: 10
-            }}
-        >
-            {listVerification && listVerification.length > 0 ? (
-                <>
-                    {listVerification.map((item) => (
-                        <View
-                            key={item.id}
-                        >
-                            {renderVerificationRequestItem(item)}
-                        </View>
-                    ))}
-                </>
-            ) : (
-                <CustomText
-                    style={{
-                        textAlign: 'center',
-                    }}
-                    text="Không có dữ liệu"
-                />
-            )}
-        </View>
-    );
+    const renderScene = SceneMap({
+        listVerificationForApplyCustomer: listVerificationForApplyPartnerRoute,
+        listVerificationForApplyPartner: listVerificationForApplyCustomerRoute,
+    });
 
     try {
         return (
-            <>
-                {isShowSpinner ? (
-                    <CenterLoader />
-                ) : (
-                    <ScrollView
-                        contentContainerStyle={{
-                            width: SIZES.WIDTH_BASE,
-                            minHeight: SIZES.HEIGHT_BASE
-                        }}
-                        refreshControl={(
-                            <RefreshControl
-                                refreshing={refreshing}
-                                onRefresh={() => onRefresh()}
-                                tintColor={COLORS.ACTIVE}
-                            />
-                        )}
-                    >
-                        {renderListWaitingVerificationRequest()}
-                    </ScrollView>
-                )}
-            </>
+            <TopTabBar
+                routes={routes}
+                renderScene={renderScene}
+                tabActiveIndex={route?.params?.tabActive || 0}
+            />
         );
     } catch (exception) {
         console.log('exception :>> ', exception);
